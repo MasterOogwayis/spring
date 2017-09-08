1bc6227934adb72647b9f0a7966b8080824322f7
zhangshaowei <zhangshaowei>
2017-09-08 10:02:47 +0800

    update

--- zipkin/server-zipkin/src/main/java/com/ServerZipkinApplication.java
+++ zipkin/server-zipkin/src/main/java/com/ServerZipkinApplication.java
@@ -2,10 +2,12 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.sleuth.stream.SleuthStreamAutoConfiguration;
 import zipkin.server.EnableZipkinServer;
 
 @EnableZipkinServer
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = SleuthStreamAutoConfiguration.class)
 public class ServerZipkinApplication {
 
