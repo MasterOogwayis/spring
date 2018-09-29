49e8f157d6ca53d8b0153b49c4b81a015c927637
zsw <499504777@qq.com>
2018-09-29 16:45:26 +0800

    emmm...

--- server/api-server/src/test/java/com/StaticTests.java
+++ server/api-server/src/test/java/com/StaticTests.java
@@ -2,13 +2,14 @@ package com;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.apache.commons.codec.binary.StringUtils;
-import org.apache.tomcat.util.codec.binary.Base64;
-import org.springframework.util.Base64Utils;
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -35,29 +36,65 @@ public class StaticTests {
 
     public static void main(String[] args) throws Exception {
 
-        String str = "eyJ0eXBlIjowLCJtYXhSZXN1bHQiOjEwLCJpZCI6LTEsInRva2VuIjoiUVNOR1NrNjlyYmRMc1Z4YklxditMNmpvTDBIYnFuNHFmSlFldGVhak41TitxSmJIRS9jK2lRPT0ifQ=";
-        String data = "{\"type\":0,\"maxResult\":10,\"id\":-1,\"token\":\"QSNGSk69rbdLsVxbIqv+L6joL0Hbqn4qfJQeteajN5N+qJbHE/c+iQ==\"}";
+//        System.err.println(Boolean.valueOf("true"));
 
+        Map<String, String> map = new HashMap<>();
+        System.out.println(Base64.encodeBase64String(gson.toJson(map).getBytes()));
+        String str = "eyJuaWNrTmFtZSI6Ik1hc3Rlck9vZ3dheSIsInNleCI6MSwiaGVhZEltZ1VybCI6Imh0dHA6Ly90aGlyZHd4LnFsb2dvLmNuL21tb3Blbi92aV8zMi9RMGo0VHdHVGZUSnlPbURWUFNBMHhmMDVNQW5VQ3dCcHFqOU9pYTJ2V3Bob2dsT3hBcWlibDYxU3JsZ1EwOWxyS1dlOHprWnNMazJQQnZuNE55TktzNHJRLzEzMiIsInJlYWxOYW1lIjoi5byg5bCR5LyfIiwibW9iaWxlIjoiMTgzKioqKjA5ODAiLCJpZENhcmQiOiI1MTAxODIqKioqKioqKjcwMzAiLCJiYXNlVXJsIjoiaHR0cDovL3d3dy5zY3lzd3ouY29tL3JlZmMiLCJ0b2tlbiI6IklWenFpa1BMMTZUQXFFYmVWTDl6Rnl2N2pKSGFUMlRWSk83TFVUekY2dCtGNzQrcHNLbXI1YURUSnI4dUlldklHNDRUYW1uK2x2TzlpWExWenZ2WW9RXHUwMDNkXHUwMDNkIn0";
+        Map<String, String> data = gson.fromJson(
+                new String(Base64.decodeBase64(str)),
+                new TypeToken<HashMap<String, String>>() {
+                }.getType());
+        System.out.println(data);
+        data.put("baseUrl", "http://localhost/");
+        System.err.println(Base64.encodeBase64String(gson.toJson(data).getBytes()));
 
-//        System.err.println("spring Base64: " + new String(org.springframework.security.crypto.codec.Base64.encode(data.getBytes())));
-        System.out.println("apache Base64: " + Base64.encodeBase64String(data.getBytes()));
+//        String path = "{\"nickName\":\"MasterOogway\",\"sex\":1,\"realName\":\"\",\"mobile\":\"183****0980\",\"idCard\":\"\",\"token\":\"ABJSAOlp2wik5AwvBp0hy4rOK0Sk+3a/4G+C4Y/aPBlBdY5t3qV1vVNzPSgk1TZ4msu+vA87pdvegXqwXR/dVQ\\u003d\\u003d\"}";
+//        String data = Base64.encodeBase64String(path.getBytes());
+//        System.err.println(data);
+//        System.out.println(new String(Base64.decodeBase64(data)));
 
-        System.err.println(new String(Base64Utils.decode(Base64.encodeBase64String(data.getBytes()).getBytes())));
+//        Files.createDirectories(Paths.get("/data/asdasdasd/"));
+//        Files.copy(
+//                Paths.get(path),
+//                Paths.get("/data/asdasdasd/3.html"),
+//                StandardCopyOption.REPLACE_EXISTING
+//        );
+    }
 
-//        System.out.println(StringUtils.newStringUtf8(org.springframework.security.crypto.codec.Base64.encode(data.getBytes())));
-//        System.out.println(Base64.encodeBase64String(data.getBytes()));
-//        System.out.println(new String(Base64.decodeBase64(str)));
-//        System.err.println(new String(Base64.decodeBase64(Base64.encodeBase64String(data.getBytes()))));
+    public static byte[] readFileByBytes(String filePath) throws IOException {
+        File file = new File(filePath);
+        if (!file.exists()) {
+            throw new FileNotFoundException(filePath);
+        } else {
+            ByteArrayOutputStream bos = new ByteArrayOutputStream((int) file.length());
+            BufferedInputStream in = null;
 
+            try {
+                in = new BufferedInputStream(new FileInputStream(file));
+                int bufSize = 1024;
+                byte[] buffer = new byte[bufSize];
+                boolean var6 = false;
 
-//        String encode = new BASE64Encoder().encode(data);
-//
-//        System.out.println(encode);
-//
-//        byte[] data1 = new BASE64Decoder().decodeBuffer(encode);
-//
-//        Files.write(Paths.get("C:\\Users\\ZhangShaowei\\Desktop\\2.jpg"), data1, StandardOpenOption.CREATE_NEW);
+                int len;
+                while (-1 != (len = in.read(buffer, 0, bufSize))) {
+                    bos.write(buffer, 0, len);
+                }
 
+                byte[] var7 = bos.toByteArray();
+                return var7;
+            } finally {
+                try {
+                    if (in != null) {
+                        in.close();
+                    }
+                } catch (IOException var14) {
+                    var14.printStackTrace();
+                }
+
+                bos.close();
+            }
+        }
     }
 
     @Data
