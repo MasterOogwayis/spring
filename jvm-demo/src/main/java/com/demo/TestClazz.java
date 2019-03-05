75288bd1224291db149d263cd9dfea0487eaf984
zhangshaowei <zhangshaowei>
2019-03-05 23:48:55 +0800

    jvm test

---
+++ jvm-demo/src/main/java/com/demo/TestClazz.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+/**
+ * @author Shaowei Zhang on 2019/3/5 21:15
+ **/
+public class TestClazz {
+
+    private int i;
+
+    public int incr(){
+        return i + 1;
+    }
+
+
+}
