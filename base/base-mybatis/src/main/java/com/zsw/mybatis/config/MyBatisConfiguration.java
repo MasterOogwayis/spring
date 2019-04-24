5735c9cae5a9c72498df6026b21255e7a2ad1aec
zsw <499504777@qq.com>
2019-04-24 11:30:52 +0800

    update

---
+++ base/base-mybatis/src/main/java/com/zsw/mybatis/config/MyBatisConfiguration.java
@@ -0,0 +1,12 @@
+package com.zsw.mybatis.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ZhangShaowei on 2019/4/24 10:48
+ **/
+@Configuration
+//@MapperScan("com.zsw.persistemce.dao")
+public class MyBatisConfiguration {
+}
