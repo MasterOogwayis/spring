736e4ede2810edada0875dc4868a8125928c3047
zsw <499504777@qq.com>
2019-06-19 14:48:40 +0800

    嘿嘿嘿

--- base/base-redis/src/main/java/org/springframework/cache/annotation/EnableCustomCaching.java
+++ base/base-redis/src/main/java/org/springframework/cache/annotation/EnableCustomCaching.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Import;
 import java.lang.annotation.*;
 
 /**
+ * TODO 自定义控制时间的注解需要 开启同名bean 覆盖 spring.main.allow-bean-definition-overriding: true 不然你得改全家桶代码
+ *
  * @author ZhangShaowei on 2019/6/19 13:39
  **/
 @Target(ElementType.TYPE)
