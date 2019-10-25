57fd1f5e2ab8d62f810d4ddfdf108e3dffe9fd5f
zsw <499504777@qq.com>
2019-10-25 09:19:21 +0800

    版本冲突

--- test/src/test/java/com/StaticTests.java
+++ test/src/test/java/com/StaticTests.java
@@ -1,7 +1,12 @@
 package com;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,31 +17,35 @@ import java.util.List;
 public class StaticTests {
 
     public static void main(String[] args) {
-        List<Dto> list = new ArrayList<>();
-        list.add(new Dto(1L, 2));
-        list.add(new Dto(1L, 1));
-        list.add(new Dto(1L, 4));
-        list.add(new Dto(1L, 3));
-        list.add(new Dto(2L, 2));
-        list.add(new Dto(2L, 2));
-        list.add(new Dto(2L, 2));
-        list.add(new Dto(2L, 2));
-        list.add(new Dto(3L, 2));
-        list.add(new Dto(3L, 2));
-        list.add(new Dto(3L, 2));
-        list.add(new Dto(3L, 2));
+//        List<Dto> list = new ArrayList<>();
+//        list.add(new Dto(1L, 2));
+//        list.add(new Dto(1L, 1));
+//        list.add(new Dto(1L, 4));
+//        list.add(new Dto(1L, 3));
+//        list.add(new Dto(2L, 2));
+//        list.add(new Dto(2L, 2));
+//        list.add(new Dto(2L, 2));
+//        list.add(new Dto(2L, 2));
+//        list.add(new Dto(3L, 2));
+//        list.add(new Dto(3L, 2));
+//        list.add(new Dto(3L, 2));
+//        list.add(new Dto(3L, 2));
+
+        System.out.println(String.join(":", "a", "b", "c", "d"));
 
 
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+
+        Dto d = new Dto();
 
     }
 
-
-
-    @Data
-    @AllArgsConstructor
+    @Getter
+    @Setter
     static class Dto {
-        private Long id;
-        private Integer index;
+        private boolean isParent;
     }
 
 }
