414d204240bdf09ceb3cbffd8f7052c96ae23138
zsw <499504777@qq.com>
2020-05-07 10:49:58 +0800

    mybatis 相关

---
+++ spring-apps/simple-test/src/test/java/com/zsw/TestControllerTest.java
@@ -0,0 +1,41 @@
+package com.zsw;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * TestController Tester.
+ *
+ * @author zsw
+ * @version 1.0
+ * @since 2020-5-6
+ */
+public class TestControllerTest {
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * @see TestController#ping()
+     */
+    @Test
+    public void testPing() throws Exception {
+        //TODO: Test goes here... 
+    }
+
+    /**
+     * @see TestController#get(String name)
+     */
+    @Test
+    public void testGet() throws Exception {
+        //TODO: Test goes here... 
+    }
+
+
+} 
