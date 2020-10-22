ad944430eb398cb5011d904002d6bdc1548e0893
zsw <499504777@qq.com>
2020-10-22 15:41:42 +0800

    vertx tests

---
+++ vertx-apps/vertx-api/src/main/java/com/zsw/VertxApiApp.java
@@ -0,0 +1,28 @@
+package com.zsw;
+
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerRequest;
+
+/**
+ * @author ZhangShaowei on 2020/10/22 15:37
+ */
+public class VertxApiApp {
+
+    public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx(new VertxOptions().setWorkerPoolSize(10));
+
+        HttpServer httpServer = vertx.createHttpServer();
+
+        httpServer.requestHandler(request -> {
+            request.response().putHeader("Content-Type", "text/plain").write("Hello World").end();
+        });
+
+        httpServer.listen(8080);
+
+
+    }
+
+}
