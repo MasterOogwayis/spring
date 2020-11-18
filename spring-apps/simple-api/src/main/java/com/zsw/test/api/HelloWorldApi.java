c73c4cfce1ab825d7d2a2380f38dd1fb7c1eb220
zsw <499504777@qq.com>
2020-11-18 16:22:30 +0800

    update

--- spring-apps/simple-api/src/main/java/com/zsw/test/api/HelloWorldApi.java
+++ spring-apps/simple-api/src/main/java/com/zsw/test/api/HelloWorldApi.java
@@ -1,24 +1,51 @@
 package com.zsw.test.api;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.nio.file.Paths;
+
 /**
  * @author ZhangShaowei on 2020/5/11 14:18
  */
 @RestController
-@RequestMapping("hello")
+@RequestMapping("/test")
 public class HelloWorldApi {
 
     @Value("${random.int[9000,10000]}")
     private String random;
 
-    @GetMapping
+    @GetMapping("hello")
     public String world(@RequestParam("name") String name) {
         return "Hello " + name + random;
     }
+
+    @GetMapping("download")
+    public FileSystemResource download1() {
+        return new FileSystemResource(Paths.get("/data/1.txt"));
+    }
+
+    @GetMapping("download")
+    public ResponseEntity<FileSystemResource> download() {
+        FileSystemResource resource = new FileSystemResource(Paths.get("/data/1.txt"));
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", resource.getFilename()));
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(resource.getFile().length())
+                .contentType(MediaType.parseMediaType("application/octet-stream"))
+                .body(resource);
+    }
+
 
 }
