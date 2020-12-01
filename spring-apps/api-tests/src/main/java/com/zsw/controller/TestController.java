9ccf3ddf8aa17997da7b742fe7e8fec68f43f969
zsw <499504777@qq.com>
2020-12-01 13:45:13 +0800

    update

--- spring-apps/api-tests/src/main/java/com/zsw/controller/TestController.java
+++ spring-apps/api-tests/src/main/java/com/zsw/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.zsw.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    private Environment environment;
+
 
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
 
     @GetMapping
     public Object get(@RequestParam("key") String key) {
-        return this.redisTemplate.opsForValue().get(key);
+        return this.environment.getProperty(key);
     }
 
 
