7ea8212d7bf44186e0f4f7f3dfcefbda8b12d432
zsw <499504777@qq.com>
2020-12-02 17:16:50 +0800

    update

--- spring-apps/api-tests/src/test/java/StaticTests.java
+++ spring-apps/api-tests/src/test/java/StaticTests.java
@@ -13,10 +13,8 @@ public class StaticTests {
     private static int x = 100;
 
     public static void main(String[] args) {
-
         System.out.println(NettyRuntime.availableProcessors());
         System.err.println(Runtime.getRuntime().availableProcessors());
-
     }
 
 
