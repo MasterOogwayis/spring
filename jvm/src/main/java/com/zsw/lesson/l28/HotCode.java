e80233305614f98cd327600fa5f44f6666689023
zsw <499504777@qq.com>
2021-06-01 14:34:20 +0800

    update

---
+++ jvm/src/main/java/com/zsw/lesson/l28/HotCode.java
@@ -0,0 +1,47 @@
+package com.zsw.lesson.l28;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.UUID;
+
+/**
+ * @author ZhangShaowei on 2021/6/1 14:27
+ */
+public class HotCode {
+
+    private static volatile int value = 0;
+
+    private static Object array;
+
+    public static void main(String[] args) {
+        while (true) {
+            hotMethod1();
+            hotMethod2();
+            hotMethod3();
+            allocate();
+        }
+    }
+
+    private static void allocate() {
+        array = new int[6 * 1000];
+        array = new Integer[6 * 1000];
+    }
+
+    private static void hotMethod3() {
+        ArrayList<String> list = new ArrayList<>();
+        UUID uuid = UUID.randomUUID();
+        String str = uuid.toString().replace("-", "");
+        list.add(str);
+
+    }
+
+    private static void hotMethod2() {
+        value ++;
+    }
+
+    private static void hotMethod1() {
+        int anInt = new Random().nextInt();
+    }
+    
+
+}
