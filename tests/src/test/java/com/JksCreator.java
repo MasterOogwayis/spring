00d7b74d0841210c94981439c98c29f9a4b9049f
zsw <499504777@qq.com>
2021-06-02 15:26:02 +0800

    update

---
+++ tests/src/test/java/com/JksCreator.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @author ZhangShaowei on 2021/6/2 14:07
+ */
+public class JksCreator {
+
+    public static void main(String[] args) {
+        // 注意主密码和store 密码要想同
+//        keytool -genkeypair -alias oauth2 -keyalg RSA -keystore oauth2.jks -keysize 2048
+    }
+
+}
