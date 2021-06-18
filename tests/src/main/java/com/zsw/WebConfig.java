23bf9f75d9e9c2e9e5da82e296c72517c9c1e4ce
zsw <499504777@qq.com>
2021-06-18 12:17:11 +0800

    update

---
+++ tests/src/main/java/com/zsw/WebConfig.java
@@ -0,0 +1,18 @@
+package com.zsw;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author ZhangShaowei on 2021/6/18 11:45
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/file/**").addResourceLocations("file:/data/newbee/upload/");
+        registry.addResourceHandler("/path/**").addResourceLocations("classpath:/static/");
+    }
+}
