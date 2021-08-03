a2be4ba33bc5fdd3b929edf29dc5f62576d2fc39
zsw <499504777@qq.com>
2021-08-03 11:35:19 +0800

    update

---
+++ base/base-office/src/main/java/com/zsw/ExcelApp.java
@@ -0,0 +1,21 @@
+package com.zsw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ZhangShaowei on 2021/8/3 11:22
+ */
+@SpringBootApplication
+public class ExcelApp {
+
+    /**
+     * start server
+     *
+     * @param args args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelApp.class, args);
+    }
+
+}
