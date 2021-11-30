ec4a918838ddcc880adec7b1bbeb883c92cc05dd
ZhangShaowei <499504777@qq.com>
2021-11-30 15:25:19 +0800

    pdf

--- base/base-repository/src/main/java/com/zsw/audit/UserAuditor.java
+++ base/base-repository/src/main/java/com/zsw/audit/UserAuditor.java
@@ -6,11 +6,12 @@ import org.springframework.data.domain.AuditorAware;
 import java.util.Optional;
 
 /**
+ * 不建议在审计里面使用 jpa 访问那数据，会导致各种恶样的问题
+ *
  * @author ZhangShaowei on 2020/9/25 14:15
  */
 @Configuration
 public class UserAuditor implements AuditorAware<String> {
-
 
     @Override
     public Optional<String> getCurrentAuditor() {
