2c9d35921e69f9c830d2744446980ea6d4d43d89
ZhangShaowei <499504777@qq.com>
2021-12-14 15:28:48 +0800

    update

--- spring-boot-features/src/test/java/com/zsw/StaticTests.java
+++ spring-boot-features/src/test/java/com/zsw/StaticTests.java
@@ -1,20 +1,43 @@
 package com.zsw;
 
+import com.mysql.cj.util.StringUtils;
+import lombok.Cleanup;
+import lombok.SneakyThrows;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
 /**
  * @author ZhangShaowei on 2021/9/23 13:50
  */
 public class StaticTests {
 
+    @SneakyThrows
     public static void main(String[] args) {
 //        TestThisService service = new JdkProxy().getInstance(new TestThisService());
 //        System.out.println(AnnotationUtils.isCandidateClass(service.getClass(), Wrapped.class));
 
-        double a = (1d - (6 / 24d));
-        double b = (1d - (6 / 23d));
-        double c = (1d - (6 / 22d));
 
-        System.out.println(1-(a * b * c));
+        URI uri = URI.create("http://localhost:18080//hello").normalize();
+
+        URL url = uri.toURL();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.connect();
+
+        @Cleanup InputStream in = connection.getInputStream();
+
+        byte[] data = new byte[in.available()];
+
+        IOUtils.readFully(connection.getInputStream(), data);
+
+        System.out.println(StringUtils.toString(data, "utf-8"));
+
 
     }
+
 
 }
--- spring-cloud/eureka-server/src/main/java/com/zsw/EurekaServer.java
+++ spring-cloud/eureka-server/src/main/java/com/zsw/EurekaServer.java
@@ -2,12 +2,11 @@ package com.zsw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author ZhangShaowei on 2017/9/8 13:22
  */
-@EnableEurekaServer
+//@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServer {
 
