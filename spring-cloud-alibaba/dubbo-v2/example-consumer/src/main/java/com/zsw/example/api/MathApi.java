834ba408d05a1efe0b9a3875153b67742c112f42
ZhangShaowei <499504777@qq.com>
2021-12-20 11:17:08 +0800

    sb update

--- spring-cloud-alibaba/dubbo-v2/example-consumer/src/main/java/com/zsw/example/api/MathApi.java
+++ spring-cloud-alibaba/dubbo-v2/example-consumer/src/main/java/com/zsw/example/api/MathApi.java
@@ -2,6 +2,7 @@ package com.zsw.example.api;
 
 import com.zsw.service.dubbo.MathService;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.rpc.service.GenericService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author ZhangShaowei on 2020/8/31 11:20
  */
-@RequestMapping("sum")
+@RequestMapping("math")
 @RestController
 public class MathApi {
 
@@ -21,9 +22,25 @@ public class MathApi {
     @DubboReference
     private MathService mathService;
 
-    @GetMapping
+    @DubboReference(
+            interfaceClass = MathService.class,
+            generic = true,
+            check = false
+    )
+    private GenericService genericService;
+
+    @GetMapping("sum")
     public Object sum(@RequestParam("a") Integer a, @RequestParam("b") Integer b) {
         return this.mathService.sum(a, b);
+    }
+
+    @GetMapping("sum2")
+    public Object sum2(@RequestParam("a") Integer a, @RequestParam("b") Integer b) {
+        return this.genericService.$invoke(
+                "sum",
+                new String[]{Integer.class.getName(), Integer.class.getName()},
+                new Object[]{a, b}
+        );
     }
 
 }
