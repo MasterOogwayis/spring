1d937abb5c5d52c31beb0b3639cbd511ba91809c
Shaowei Zhang <499504777@qq.com>
2022-02-14 22:25:04 +0800

    解决版本问题导致的无法 import

---
+++ some-tests/src/main/java/com/zsw/utils/JdkSeriUtils.java
@@ -0,0 +1,7 @@
+package com.zsw.utils;
+
+/**
+ * @author Shaowei Zhang on 2022/2/14 22:15
+ */
+public class JdkSeriUtils {
+}
---
+++ some-tests/src/test/java/com/zsw/SomeTests.java
@@ -0,0 +1,12 @@
+package com.zsw;
+
+/**
+ * @author Shaowei Zhang on 2022/2/14 22:14
+ */
+public class SomeTests {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
