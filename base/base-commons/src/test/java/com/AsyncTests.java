eec44c069ae7103c9d4e6794b8541248450da49e
ZhangShaowei <499504777@qq.com>
2022-06-24 16:26:33 +0800

    update

--- base/base-commons/src/test/java/com/AsyncTests.java
+++ base/base-commons/src/test/java/com/AsyncTests.java
@@ -1,6 +1,7 @@
 package com;
 
 import com.zsw.function.ThrowableSupplier;
+import lombok.SneakyThrows;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
@@ -15,6 +16,7 @@ import static com.zsw.function.ThrowableExecutor.execute;
  */
 public class AsyncTests {
 
+    @SneakyThrows
     public static void main(String[] args) {
 
         ThreadPoolExecutor executor = new ThreadPoolExecutor(
@@ -27,21 +29,22 @@ public class AsyncTests {
                 new ThreadPoolExecutor.CallerRunsPolicy());
 
         CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
-
-            execute(() -> TimeUnit.SECONDS.sleep(5));
+            execute(() -> TimeUnit.SECONDS.sleep(2));
+            System.out.println("thread name: " + Thread.currentThread().getName());
             return "hello World";
-
         }, executor);
 
-        execute(() -> CompletableFuture.allOf(completableFuture).get());
+        // get() 会阻塞到所有任务执行完，只执行 allOf 只会触发任务，不会等待
+//        execute(() -> CompletableFuture.allOf(completableFuture).get());
+        CompletableFuture.allOf(completableFuture);
+
+        TimeUnit.SECONDS.sleep(3);
 
         String data = ThrowableSupplier.execute(completableFuture::get);
 
         System.out.println(data);
 
         executor.shutdownNow();
-
-
     }
 
 
---
+++ tests/src/test/java/com/tests/CompletableFutureTests.java
@@ -0,0 +1,18 @@
+package com.tests;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author ZhangShaowei on 2022/6/24 16:23
+ */
+public class CompletableFutureTests {
+
+    public static void main(String[] args) {
+        CompletableFuture.supplyAsync()
+
+
+
+    }
+
+
+}
