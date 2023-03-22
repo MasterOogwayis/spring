229ac60630a45ca2ecb86af644146d350f459e0e
Shaowei <shaowei.zhang.mail@qq.com>
2023-03-22 20:48:36 +0800

    up

---
+++ some-tests/src/main/java/com/zsw/Tetris.java
@@ -0,0 +1,11 @@
+package com.zsw;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+
+/**
+ * @author zhangshaowei on 2023/3/22 20:47
+ */
+public class Tetris extends JPanel implements ActionListener, KeyListener {
+}
